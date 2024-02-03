87253f7fbe01be44d4843704dd99f2aa07ca0bc7
frc2175-driverstation <80791748+frc2175-driverstation@users.noreply.github.com>
2024-02-03 11:08:11 -0600

    Ignore simulator and cache files

--- arm.py
+++ arm.py
@@ -3,6 +3,7 @@ import rev
 class Arm:
     def __init__(self, angleMotorId) -> None:
         self.angleMotor = rev.CANSparkMax(angleMotorId, rev.CANSparkLowLevel.MotorType.kBrushless)
+        self.angleMotor.IdleMode(1)
 
     def setArmSpeed(self, motorSpeed):
         self.angleMotor.set(motorSpeed)
